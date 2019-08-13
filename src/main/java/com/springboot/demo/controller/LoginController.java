1c44987f0029c09a5f507d9ccc82b9b274b8524d
Zkl-mango <1473055523@qq.com>
2019-08-13 14:37:11 +0800

    添加简单的登录接口

---
+++ src/main/java/com/springboot/demo/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.springboot.demo.controller;
+
+import com.springboot.demo.entity.UserEntity;
+import com.springboot.demo.service.UserService;
+import com.springboot.demo.util.RedisUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class LoginController {
+
+    @Resource
+    UserService userService;
+    @Resource
+    RedisUtil redisUtil;
+
+    @GetMapping("/login")
+    public Map<String,Object> login(String username,String password) {
+        Map<String,Object> map = new HashMap<>();
+        UserEntity user = userService.getUserByUserName(username);
+        if(!user.getPassword().equals(password)) {
+            map.put("message","密码错误");
+            map.put("status","error");
+            return map;
+        }
+        redisUtil.set(user.getId(),user);
+        map.put("message","登录成功");
+        map.put("status","accept");
+        return map;
+    }
+
+}
